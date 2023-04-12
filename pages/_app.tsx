2240c4e510680ba866c22da1c47ac8c079242f1d
Jack Dewan <jdewan16@gmail.com>
2023-04-12 16:46:25 -0400

    update gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,19 +23,21 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+      <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
         `}
-      </Script>
+        </Script>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
