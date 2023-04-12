d24c9687b2630f85edf272498a265fda35964580
Jack Dewan <jdewan16@gmail.com>
2023-04-12 17:08:02 -0400

    update gtag to <script/>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,38 +5,35 @@ import Head from "next/head";
 import Script from "next/script";
 
 // import { GA_MEASUREMENT_ID } from "../lib/gtag";
-import { useRouter } from "next/router";
+import Router from "next/router";
 import { useEffect } from "react";
 import * as gtag from "../lib/gtag";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url) => {
       gtag.pageview(url);
     };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+    Router.events.on("routeChangeComplete", handleRouteChange);
+  });
 
   return (
     <>
       <Head>
-        <Script
+        <script
+          async
           src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
           window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
-        `}
-        </Script>
+        `,
+          }}
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
