4dbc699c2b519a7809c291403736fc3f3cb9fa14
Jack Dewan <jdewan16@gmail.com>
2023-04-12 09:59:40 -0400

    add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,44 @@
 import { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "../styles/index.css";
+import Head from "next/head";
+import Script from "next/script";
+
+// import { GA_MEASUREMENT_ID } from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${gtag.GA_MEASUREMENT_ID});
+        `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
